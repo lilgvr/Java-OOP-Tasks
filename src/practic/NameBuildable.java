bc4c7d3f2426ae18899c5dfaa453bf0f813459d8
Dima Gavrilenko <d.gavrilenko262@gmail.com>
2020-11-07 13:05:05 +0400

    Some tasks in block 7

--- src/practic/NameBuildable.java
+++
@@ -1,8 +0,0 @@
-package practic;
-
-public interface NameBuildable {
-    NameBuildable setName(String name);
-    NameBuildable setMidname(String midname);
-    NameBuildable setSurname(String surname);
-    Name getName();
-}
